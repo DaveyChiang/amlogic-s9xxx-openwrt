e0671f4f3f75fd3b7cca5dae009657e1d079c61a
DaveyChiang <104084231+DaveyChiang@users.noreply.github.com>
2026-09-01 20:15:54 +0800

    Update diy-part2.sh

--- config/immortalwrt_master/diy-part2.sh
+++ config/immortalwrt_master/diy-part2.sh
@@ -9,7 +9,7 @@
 # ------------------------------- Main source configuration -------------------------------
 #
 # Set the default LAN IP address
-default_ip="192.168.1.1"
+default_ip="10.0.0.88"
 ip_regex="^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
 # Override default IP if a valid custom IP is provided as the first argument
 [[ -n "${1}" && "${1}" != "${default_ip}" && "${1}" =~ ${ip_regex} ]] && {
@@ -49,6 +49,8 @@ fi
 rm -rf package/luci-app-amlogic
 git clone -b main https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 #
+# 设置默认主题为 argon
+echo "CONFIG_PACKAGE_luci-theme-argon=y" >> .config
 # Apply patches
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
 #
